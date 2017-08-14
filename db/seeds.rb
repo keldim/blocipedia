b263dbc563c6eb26c6421c587a890ccf850090fc
Chris Yoo <woyoo@umich.edu>
2017-08-13 21:39:44 -0400

    finish blocipedia-checkpoint-6

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+   User.create!(
+   email:    Faker::Internet.unique.email,
+   password: Faker::Internet.unique.password(8),
+   role: 'standard'
+   )
+ end
+
+users = User.all
+
+ 5.times do
+    Wiki.create!(
+    title:    Faker::Company.unique.name,
+    body: Faker::Company.unique.catch_phrase,
+    private: false,
+    user: users.sample
+    )
+  end
+
+
+admin = User.create!(
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+premium = User.create!(
+  email:    'premium@example.com',
+  password: 'helloworld',
+  role: 'premium'
+)
+
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
